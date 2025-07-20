af6bbb374a2c80b41708f7f0d9f1b8173e9a8f65
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-20 17:38:37 +0600

    feat: Implement custom User model and manager for email-based auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from decimal import Decimal
+from django.contrib.auth. models import AbstractBaseUser
+from django.core.validators import (MinValueValidator, MaxLengthValidator)
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser):
+    username = None
+    email = models.EmailField(unique=True, null=False, blank=True)
+    # all user objects will be handaled by the UserManager
+    objects = UserManager
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+    
+    @property
+    def balance(self):
+        if hasattr(self, 'account'):
+            return self.account.balance
+        return 0
